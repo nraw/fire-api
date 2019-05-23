e59aede05df1224d1cbc7a87ba59feadfe9cd734
nraw <marsic.andrej@gmail.com>
2019-05-24 00:19:18 +0200

    improved test

--- test_model.py
+++ test_model.py
@@ -1,11 +1,25 @@
-from model import Model
 import os
+import pytest
+
+
+@pytest.fixture
+def model():
+    from model import Model
+    model = Model()
+    return model
 
 
 def test_db_string():
     assert os.environ['DB_URI'] != ''
 
 
-def test_model_init():
-    model = Model()
+def test_model_init(model):
     assert model.model is not None
+
+
+def test_prediction(model):
+    prediction = model.predict('Petra', 'Gayane', 'Igor', 'Felipe')
+    assert type(prediction) == type([0])
+    assert len(prediction) == 1
+    assert prediction[0] <= 1
+    assert prediction[0] >= 0
